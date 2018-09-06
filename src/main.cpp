3b08284f5378e7e84b1f6f462742446b14d00b70
m1lg <m1lg@users.noreply.github.com>
2018-09-06 20:11:01 +1000

    working with transforms

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <std_msgs/Float64.h>
 #include <sensor_msgs/JointState.h>
 #include <sensor_msgs/Imu.h>
+#include <tf/transform_listener.h>
 
 struct odom {
     double back_wheel_vel;    
@@ -15,25 +16,19 @@ void counterCallbackJoint(const sensor_msgs::JointState::ConstPtr& msg) {// Defi
                                                                          // a parameter named 'msg' 
 	std::vector<std::string> names = msg->name;
 	if (msg->name[0][0] == 'f') {
-		ROS_INFO("%s", &names[0][0]);
+	
+        ROS_INFO("%s", &names[0][0]);
         odom_data.back_wheel_vel = (msg->velocity[1]+msg->velocity[2])/2;
 		ROS_INFO("avg= %f",odom_data.back_wheel_vel);
 	} else {
-		ROS_INFO("%s", &names[0][0]);
+		
+        ROS_INFO("%s", &names[0][0]);
 	}
-	//int i, j;
-	//for (i = 0; i < names.size(); i++) {
-	//	ROS_INFO("%d", *names[i]);
-	//	for (j = 0; j < names[i].size(); j++) {	
-		//ROS_INFO("%c", names[i][j]);
-	//	}
-	//}
-	//}
 }
 
-//void counterCallbackIMU(const sensor_msgs::Imu::ConstPtr& msg) {// Define a function called 'callback' that receives a                                                                                // parameter named 'msg'
-        //ROS_INFO("x = %f\t y = %f\t z = %f\t", msg->pose.pose.position.x,msg->pose.pose.position.y,msg->pose.pose.position.z);
-//}
+void counterCallbackIMU(const sensor_msgs::Imu::ConstPtr& msg) {
+    ROS_INFO("IMU subscribe");
+}
 
 
 int main(int argc, char** argv) {
@@ -42,12 +37,14 @@ int main(int argc, char** argv) {
 	ros::NodeHandle nh;
     
     ros::Subscriber sub_joint_states = nh.subscribe("/zio/joint_states", 1000, counterCallbackJoint); 
-    //ros::Subscriber sub_imu = nh.subscribe("/vn100/imu", 1000, counterCallback);   
-    										      // Create a Subscriber object that will listen 
-    										      // to the /counter topic and will call the 
-										      // 'callback' function each time it reads 
-										      // somethin from the topic
-   
+    ros::Subscriber sub_imu = nh.subscribe("/vn100/imu", 1000, counterCallbackIMU);   
+                                                // Create a Subscriber object that will listen 
+    										    // to the /counter topic and will call the 
+										        // 'callback' function each time it reads 
+										        // somethin from the topic
+
+    tf::TransformListener tf_listen;
+
     ros::spin(); // Create a loop that will keep the program in execution
     
     return 0;
