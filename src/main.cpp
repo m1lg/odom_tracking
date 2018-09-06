843221359b2bbd51c30af4f35c5a0003f3c60ff7
m1lg <m1lg@users.noreply.github.com>
2018-09-07 09:54:14 +1000

    wait until static transform received before creating subscriber.

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 #include <std_msgs/Float64.h>
 #include <sensor_msgs/JointState.h>
 #include <sensor_msgs/Imu.h>
-#include <tf/transform_listener.h>
+#include <tf2_ros/transform_listener.h>
+
 
 struct odom {
     double back_wheel_vel;    
@@ -33,19 +34,44 @@ void counterCallbackIMU(const sensor_msgs::Imu::ConstPtr& msg) {
 
 int main(int argc, char** argv) {
     	
-	ros::init(argc, argv, "topic_subscriber"); // Initiate a Node called 'topic_subscriber'
+	bool tf2Received = 0;
+    ros::init(argc, argv, "odom_tracking"); // Initiate a Node called 'odom_tracking'
 	ros::NodeHandle nh;
     
-    ros::Subscriber sub_joint_states = nh.subscribe("/zio/joint_states", 1000, counterCallbackJoint); 
-    ros::Subscriber sub_imu = nh.subscribe("/vn100/imu", 1000, counterCallbackIMU);   
-                                                // Create a Subscriber object that will listen 
-    										    // to the /counter topic and will call the 
-										        // 'callback' function each time it reads 
-										        // somethin from the topic
-
-    tf::TransformListener tf_listen;
-
-    ros::spin(); // Create a loop that will keep the program in execution
+    tf2_ros::Buffer tf2Buffer;
+    tf2_ros::TransformListener tf2Listener(tf2Buffer);
     
+    while(!tf2Received) {
+
+        geometry_msgs::TransformStamped transformStamped;
+        try {
+            transformStamped = tf2Buffer.lookupTransform("base_link", "base_footprint",
+            ros::Time::now());//, ros::Duration(10.0));
+        }
+        catch (tf2::TransformException &ex) {
+            ROS_WARN("%s",ex.what());
+            ros::Duration(1.0).sleep();
+            continue;
+        }
+        tf2Received = 1;
+    }
+
+    ros::Subscriber sub_joint_states = nh.subscribe("/zio/joint_states", 1000, counterCallbackJoint);
+    ros::Subscriber sub_imu = nh.subscribe("/vn100/imu", 1000, counterCallbackIMU);
+                                               // Create a Subscriber object that will listen 
+                                               // to the /counter topic and will call the 
+                                               // 'callback' function each time it reads 
+                                               // somethin from the topic
+   
+    ros::Rate loop_rate(50); // Set a publish rate of 2 Hz
+
+    while (ros::ok()) {
+    
+        ros::spinOnce();
+        loop_rate.sleep(); // Make sure the publish rate maintains at 2 Hz
+    }
     return 0;
-}
+
+    //ros::spin(); // Create a loop that will keep the program in execution
+    
+    }
